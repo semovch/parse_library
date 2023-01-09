8e9d2d6ed9e956710d4cd6cf07e41c153e343412
semovch <loxcvetochny@gmail.com>
2023-01-10 00:19:46 +0600

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from bs4 import BeautifulSoup
 
 from urllib.parse import urljoin
 
-from requests.exceptions import HTTPError
+from requests.exceptions import HTTPError, ConnectionError
 
 from pathvalidate import sanitize_filename
 
@@ -18,20 +18,22 @@ def check_for_redirect(response):
         raise HTTPError(f'Redirectrd url: {response.url}')  
         
         
-def download_txt(url, filename, folder='books'):        
+def download_txt(url, filename, book_id, folder='books'):        
         
         os.makedirs(folder, exist_ok=True)
-        
-        response = requests.get(url)  
-        response.raise_for_status()
-              
+           
         try: 
+            params = {'id':book_id}
+            response = requests.get(url, params=params)  
+            response.raise_for_status()
             check_for_redirect(response)
             with open(os.path.join(folder, filename), 'wb') as file:
                 file.write(response.content)
-        except requests.exceptions.HTTPError as err:
+        except HTTPError as err:
             print(f'No book: {err}')
-        return(os.path.join(folder, filename))    
+        except ConnectionError as err:
+            print(f'No connection: {err}') 
+        return os.path.join(folder, filename)    
 
 
 def download_image(url, filename, folder='images'):    
@@ -40,16 +42,17 @@ def download_image(url, filename, folder='images'):
 
         os.makedirs(folder, exist_ok=True)
         
-        response = requests.get(url)  
-        response.raise_for_status()
-              
-        try: 
+        try:
+            response = requests.get(url)   
+            response.raise_for_status()
             check_for_redirect(response)
             with open(os.path.join(folder, filename), 'wb') as file:
                 file.write(response.content)
-        except requests.exceptions.HTTPError as err:
+        except HTTPError as err:
             print(f'No image: {err}')
-        return(os.path.join(folder, filename))        
+        except ConnectionError as err:
+            print(f'No connection: {err}')     
+        return os.path.join(folder, filename)        
       
                            
 def parse_book_page(book_id, page):  
@@ -63,38 +66,36 @@ def parse_book_page(book_id, page):
     book_comments = [comment.find('span', class_='black').text for comment in soup.find_all('div', class_='texts')]
         
     filename = f'{sanitize_filename(book_title)}-{sanitize_filename(book_author)}'
-    txt_url = f'https://tululu.org/txt.php?id={book_id}' 
+    txt_url = f'https://tululu.org/txt.php' #?id={book_id} 
         
-    return({'filename':filename,
+    return {'filename':filename,
             'txt_url': txt_url,
             'book_image':book_image,
             'book_genres':book_genres,
-            'book_comments':book_comments})
+            'book_comments':book_comments}
 
     
 def download_books(start_id, end_id):
     for book_id in range(start_id, end_id+1):
         
         book_url = f'https://tululu.org/b{book_id}/'
-        
-        
-        response = requests.get(book_url)
-        
-        response.raise_for_status()
-        
+       
         try: 
+            response = requests.get(book_url)
+            response.raise_for_status()
             check_for_redirect(response)
             parsed_page = parse_book_page(book_id, response)
             book_filename = f'{parsed_page["filename"]}.txt'
             image_filename = f'{parsed_page["filename"]}'
             txt_url = parsed_page['txt_url']
             image_url = parsed_page['book_image']
-            download_txt(txt_url, book_filename)
-            download_image(image_url, image_filename)
-            
+            download_txt(txt_url, book_filename, book_id)
+            download_image(image_url, image_filename)            
         except requests.exceptions.HTTPError as err:
             print(f'No page: {err}')   
-        
+        except requests.exceptions.ConnectionError as err:
+            print(f'No connection: {err}')       
+       
 def main():
     parser = argparse.ArgumentParser(
        description='Download books from tululu'
@@ -105,7 +106,7 @@ def main():
     start_id = args.start_id
     end_id = args.end_id
     download_books(start_id, end_id)
-  
+       
     
 if __name__ == '__main__':
     main()        
