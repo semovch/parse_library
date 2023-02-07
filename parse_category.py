c3e91e3c38195c3bf759635f9b51c771bf9f9934
semovch <loxcvetochny@gmail.com>
2023-02-08 01:17:28 +0600

    Update parse_category.py

--- parse_category.py
+++ parse_category.py
@@ -6,7 +6,7 @@ import argparse
 
 from bs4 import BeautifulSoup
 from urllib.parse import urljoin
-from tululu_download import download_txt, download_image, parse_book_page
+from tululu_download import download_txt, download_image, parse_book_page, check_for_redirect
 
 
 def download_many_books(soup, json_path, dest_folder, genre_id, skip_txt, skip_imgs):
@@ -18,6 +18,7 @@ def download_many_books(soup, json_path, dest_folder, genre_id, skip_txt, skip_i
             book_url = urljoin('https://tululu.org',book_href.get('href'))
             book_response = requests.get(book_url)
             book_response.raise_for_status()
+            check_for_redirect(book_response)
             parsed_page = parse_book_page(book_response)
             book_filename = f'{parsed_page["filename"]}.txt'
             image_filename = f'{parsed_page["filename"]}'
@@ -29,23 +30,21 @@ def download_many_books(soup, json_path, dest_folder, genre_id, skip_txt, skip_i
              
                 book_path = download_txt(txt_url, book_filename, book_id, folder = os.path.join(dest_folder, 'books'))
             
-            book_info = {'title': parsed_page['book_title'],
-                         'author': parsed_page['book_author'],
-                         'img_src': parsed_page['image_src'],
-                         'book_path': book_path,
-                         'comments': parsed_page['book_comments'],
-                         'genres': parsed_page['book_genres']}
+            book_params = {'title': parsed_page['book_title'],
+                           'author': parsed_page['book_author'],
+                           'img_src': parsed_page['image_src'],
+                           'book_path': book_path,
+                           'comments': parsed_page['book_comments'],
+                           'genres': parsed_page['book_genres']}
                  
             json_path_and_name = os.path.join(dest_folder, f'books_info{genre_id}.json')                  
             if json_path != '':
                 os.makedirs(json_path, exist_ok=True)
-                json_path_and_name = os.path.join(json_path, 'books_info{genre_id}.json')                                               
-            book_info_json = json.dumps(book_info, indent=4, ensure_ascii=False)                 
-            with open(json_path_and_name, 'a', encoding='utf_8') as my_file:
-                my_file.write(f'{book_info_json}\n') 
-            if not skip_imgs: 
+                json_path_and_name = os.path.join(json_path, 'books_info{genre_id}.json') 
+            with open(json_path_and_name, 'a', encoding='utf_8') as json_file:
+                json.dump(book_params, json_file, indent=4, ensure_ascii=False)     
+            if not skip_imgs and image_url != 'https://tululu.org/images/nopic.gif': 
                 download_image(image_url, image_filename, folder = os.path.join(dest_folder, 'images')) 
-            print(book_url)
         except requests.exceptions.HTTPError as err:
             print(f'No resource: {err}')   
         except requests.exceptions.ConnectionError as err:
@@ -78,11 +77,18 @@ def main():
     
     for page in range(start_page, end_page+1):
         url_of_genre = f'https://tululu.org/l{genre_id}/{page}/'
-        page = requests.get(url_of_genre)
-        page.raise_for_status()
-        soup = BeautifulSoup(page.text, 'lxml')
-        download_many_books(soup, json_path, dest_folder, genre_id, skip_txt, skip_imgs)
-    
+        try:
+            page = requests.get(url_of_genre)
+            page.raise_for_status()
+            check_for_redirect(page)
+            soup = BeautifulSoup(page.text, 'lxml')
+            download_many_books(soup, json_path, dest_folder, genre_id, skip_txt, skip_imgs)
+        except requests.exceptions.HTTPError as err:
+            print(f'No resource: {err}')   
+        except requests.exceptions.ConnectionError as err:
+            time.sleep(10)
+            print(f'No connection: {err}')   
+            
 if __name__ == '__main__':
     main()   
      
